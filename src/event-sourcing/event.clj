6565569079beab79b7bb41f116d8e66b0c0712ca
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2017-01-07 19:42:35 +0100

    -

--- src/event-sourcing/event.clj
+++ src/event-sourcing/event.clj
@@ -1,6 +1,14 @@
 (ns events)
 
 
+(defn validate-attributes
+  "Ensures that the event has only the specified attributes"
+  [keys attributes]
+  (let [keys-as-symbols (map keyword keys)
+        as-map          (apply hash-map attributes)
+        filtered        (select-keys as-map keys-as-symbols)]
+    (assert (every? filtered keys-as-symbols) "Required keus missing")
+    filtered))
 
 (defmacro defevent
   "Defines an event constructor of a unique type"
@@ -8,7 +16,7 @@
   `(defn ~type [& values#]
      {:type  ~(keyword type)
       :date   (java.util.Date.)
-      :attrs  (select-keys (apply hash-map values#) ~(map keyword keys))}))
+      :attrs  (validate-attributes '~keys values#)}))
 
 
 
@@ -16,4 +24,4 @@
 (defmethod process-event :default [state {:keys [type] :as event}]
   (case type
     :arrival   state
-               state))
+    state))
