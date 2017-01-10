8df2f344a5e9e582b9a70c7e97d2d6320e2d2fe6
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-10 14:40:10 +0100

    variants

---
+++ src/variants/variants.clj
@@ -0,0 +1,10 @@
+(ns variants
+ (:require [clojure.core.match :refer [match]]))
+
+(def image {:image/in-memory [[0 1 0 1]]})
+
+(defn display [image]
+  (match [image]
+    [{:image/in-memory data}] (println "data: "data)
+    [{:image/on-disk   path}] (println "path: "path)
+    [{:image/web       url}]  (println "url:  "url)))
