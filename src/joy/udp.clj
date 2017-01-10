bbf933b35ebe024740a0ad61a673a39574fe9d32
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-10 14:12:31 +0100

    polymorphism

--- src/joy/udp.clj
+++ src/joy/udp.clj
@@ -1,1 +1,14 @@
-(ns joy.udp)
+(ns joy.udp
+  (:refer-clojure :exclude [get]))
+
+(defn beget [this proto]
+  (assoc this ::prototype proto))
+
+(defn get [m k]
+  (when m
+    (if-let [[_ v] (find m k)]
+      (if v
+        v
+        (recur (::prototype m) k)))))
+
+(def put assoc)
