c6a4de6dcad3cbf1e84f152cef728551ffb8d9aa
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-13 17:33:57 +0100

    tests

--- test/db/core.clj
+++ test/db/core.clj
@@ -2,16 +2,13 @@
   (:use midje.sweet)
   (:require [db.core :refer [transition event]]))
 
-(defn transition-event [& args]
-  (transition #{} (apply event args)))
-
 (facts "about event"
   (event :assert {:color :blue}) => (contains {:type :assert :attributes {:color :blue}})
   (event :retract [:drink] :where {:drink :tea}) => (contains {:where {:drink :tea}}))
 
 (facts "about transition"
   ; Insert
-  (transition-event :assert {:color :blue}) => #{{:color :blue}}
+  (transition #{} (event :assert {:color :blue})) => #{{:color :blue}}
   ; Update
   (transition #{{:drink :tea}} (event :assert {:drink :coffee} :where {:drink :tea})) => #{{:drink :coffee}}
   (transition #{{:age 4 :name "Lieuwe"}} (event :assert {:age 5} :where {:name "Lieuwe"})) => #{{:age 5 :name "Lieuwe"}}
