49da4ae46b79fc5baf4aa6a332446ad334317ffe
reinvdwoerd <reinvanderwoerd@me.com>
2017-02-10 14:03:02 +0100

    api

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
      [org.clojure/core.incubator "0.1.4"]
      [org.clojure/core.async "0.2.395"]
      [org.clojure/core.match "0.3.0-alpha4"]
+     [compojure "1.6.0-beta3"]
+     [http-kit "2.3.0-alpha1"]
      [proto-repl "0.3.1"]]
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["src" "test"]
