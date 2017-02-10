6f3b895c8b8e22426c2706d0fcea5a8225e8bde6
reinvdwoerd <reinvanderwoerd@me.com>
2017-02-10 13:59:03 +0100

    -

--- src/sandbox/example.clj
+++ src/sandbox/example.clj
@@ -4,5 +4,11 @@
 
 (s/def ::job-id int?)
 
+(defevent user-registered
+  :req [::username ::password])
+
 (defevent session-started
   :req [::job-id ::user-id])
+
+(def events [(user-registered ::username "me" ::password nil)
+             (session-started ::job-id 1 ::user-id 2)])
