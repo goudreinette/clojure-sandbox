cbb26d0c016a2d811b0df0adb2c33b613212c7e9
reinvdwoerd <reinvanderwoerd@me.com>
2017-03-05 16:46:25 +0100

    core.async + multimethods

---
+++ src/sandbox/async.clj
@@ -0,0 +1,7 @@
+(ns sandbox.async
+  (:require [clojure.core.async :refer [go-loop]]))
+
+(defmacro forever [& body]
+ `(go-loop []
+    ~@body
+    (recur)))
---
+++ src/sandbox/async_handling.clj
@@ -0,0 +1,19 @@
+(ns sandbox.async-handling
+  (:require [clojure.core.async :refer [>! <! chan >!! <!!]]
+            [sandbox [async :refer :all]
+                     [multi :refer [defmethods]]]))
+
+(def actions (chan))
+(defonce todos (atom []))
+
+
+(defmulti handle first)
+
+(defmethods handle [[type payload]]
+  :todos/add
+    (swap! todos conj payload)
+  :default
+    (println "Unknown action type."))
+
+(forever
+  (handle (<! actions)))
