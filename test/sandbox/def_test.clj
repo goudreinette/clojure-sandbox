22e51e728dc13c122285335056b602325ebad78d
reinvdwoerd <reinvanderwoerd@me.com>
2017-03-06 12:55:45 +0100

    defmacro-for

--- test/sandbox/def_test.clj
+++ test/sandbox/def_test.clj
@@ -2,9 +2,10 @@
   (:use midje.sweet sandbox.def))
 
 (defmacro-for partial)
+(defmacro-for comp)
 (defpartial plus3 + 3)
-(defpartial twice * 2)
+(defcomp plus6 plus3 plus3)
 
 (facts "about defmacro-for"
   (plus3 1) => 4
-  (twice 3) => 6)
+  (plus6 3) => 9)
