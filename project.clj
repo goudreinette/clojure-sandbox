692a898bf1965df30562b610b6c24d73d495397c
reinvdwoerd <reinvanderwoerd@me.com>
2017-04-01 16:08:58 +0200

    BF

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
 (defproject reinvdwoerd/sandbox "0.0.1-SNAPSHOT"
   :description ""
   :dependencies
-    [[org.clojure/clojure "1.9.0-alpha14"]
-     [org.clojure/core.incubator "0.1.4"]
-     [org.clojure/core.async "0.2.395"]
-     [org.clojure/core.match "0.3.0-alpha4"]
-     [compojure "1.6.0-beta3"]
-     [http-kit "2.3.0-alpha1"]
-     [prismatic/schema "1.1.3"]
-     [com.rpl/specter "1.0.0"]
-     [proto-repl "0.3.1"]
-     [overtone "0.10.1"]]
+  [[org.clojure/clojure "1.9.0-alpha14"]
+   [org.clojure/core.incubator "0.1.4"]
+   [org.clojure/core.async "0.2.395"]
+   [org.clojure/core.match "0.3.0-alpha4"]
+   [compojure "1.6.0-beta3"]
+   [http-kit "2.3.0-alpha1"]
+   [prismatic/schema "1.1.3"]
+   [com.rpl/specter "1.0.0"]
+   [instaparse "1.4.5"]]
   :profiles {:uberjar {:aot :all}
-             :dev {:source-paths ["src" "test"]
-                   :dependencies [[midje "1.9.0-alpha6"]]}})
+             :dev     {:source-paths ["src" "test"]
+                       :dependencies [[midje "1.9.0-alpha6"]]}})
