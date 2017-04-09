10b420188465f86cf1668687059e827dfeff9eeb
reinvdwoerd <reinvanderwoerd@me.com>
2017-04-09 16:56:51 +0200

    do-let test

--- test/sandbox/control_flow_test.clj
+++ test/sandbox/control_flow_test.clj
@@ -1,6 +1,6 @@
 (ns sandbox.control-flow-test
-  (:use midje.sweet)
-  (:require [sandbox.control-flow :refer [for-fold]]))
+  (:refer-clojure :exclude [ensure])
+  (:use midje.sweet sandbox.control-flow))
 
 (facts "for-fold works like reduce"
   (for-fold [sum 0
@@ -11,3 +11,9 @@
   (for-fold [frequencies {}
              char "hello world"]
     (update frequencies char (fnil inc 0))) => {\h 1, \e 1, \l 3, \o 2, \space 1, \w 1, \r 1, \d 1})
+
+(facts "do-let is just let where the bindings come last"
+  (do-let
+    :ignore-me
+    (+ a b)
+    [a 1 b 2]) => 3)
