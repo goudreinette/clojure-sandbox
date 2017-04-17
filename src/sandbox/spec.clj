3013a38e5194046eaf15fe8b62f6fab4a2104e77
reinvdwoerd <reinvanderwoerd@me.com>
2017-04-17 13:51:12 +0200

    defmapspec

--- src/sandbox/spec.clj
+++ src/sandbox/spec.clj
@@ -4,5 +4,13 @@
 (defmacro defspecs
   "Define multiple clojure.spec spec's"
   [& {:as specs}]
- `(do ~@(for [[kw spec] specs]
-            `(s/def ~kw ~spec))))
+  `(do ~@(for [[kw spec] specs]
+           `(s/def ~kw ~spec))))
+
+(defmacro defmapspec
+  "Define specs for a map and it's keys"
+  [map-name & specs]
+  `(defspecs
+     ~map-name (s/keys :req [~@(map first (partition 2 specs))])
+     ~@specs))
+
--- test/sandbox/spec_test.clj
+++ test/sandbox/spec_test.clj
@@ -7,7 +7,17 @@
   ::age int?
   ::person (s/keys :req [::name ::age]))
 
+(defmapspec ::dog
+  ::favorite-food string?
+  ::dog-age int?)
+
 (facts "about defspecs"
   (s/describe ::name) => 'string?
   (s/describe ::age) => 'int?
   (s/describe ::person) => '(keys :req [::name ::age]))
+
+
+(facts "about defmapspec"
+  (s/describe ::favorite-food) => 'string?
+  (s/describe ::dog-age) => 'int?
+  (s/describe ::dog) => '(keys :req [::favorite-food ::dog-age]))
